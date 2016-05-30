c92651b3121a3fec15f80793189aa5f47cb8d422
gernest <geofreyernest@live.com>
2016-05-30 15:41:26 +0300

    Add config.go
    Define configuration files and the configuration file loader. Yaml configuration
    file format is the one supported for now

---
+++ config.go
@@ -0,0 +1,41 @@
+package valor
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+const configFile = "_valor.yml"
+
+//Config is the configuration object for valor. The supported format is yaml.
+type Config struct {
+	WorkingDir   string          `yaml:"working_dir"`
+	BaseURL      string          `yaml:"base_url"`
+	DataDir      string          `yaml:"data_dir"`
+	SourceDir    string          `yaml:"source_dir"`
+	OutputDir    string          `yaml:"output_dir"`
+	PluginDir    string          `yaml:"plugin_dir"`
+	ThemesDir    string          `yaml:"themes_dir"`
+	Handlers     []HandlerConfig `yaml:"handlers"`
+	Boot         []string        `yaml:"boot"`
+	ShuttingDown []string        `yaml:"shutting_down"`
+}
+
+type HandlerConfig struct {
+	Pattern  string   `yaml:"pattern"`
+	Renderer string   `yaml:"renderer"`
+	Plugins  []string `yaml:"plugin"`
+}
+
+func loadConfig(root string) (*Config, error) {
+	cfgFile := filepath.Join(root, configFile)
+	data, err := ioutil.ReadFile(cfgFile)
+	cfg := &Config{}
+	err = yaml.Unmarshal(data, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
