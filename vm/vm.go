df7a81354084c1a99fe0688428264658f2ad2e65
gernest <geofreyernest@live.com>
2016-06-11 23:21:35 +0300

    Rewrite VM
    Use the *Require instance to setup require glabal

--- vm/vm.go
+++ vm/vm.go
@@ -1,26 +1,13 @@
 package vm
 
-import (
-	"errors"
-
-	"github.com/ddliu/motto"
-	"github.com/robertkrimen/otto"
-)
+import "github.com/robertkrimen/otto"
 
 type VM struct {
-	vm      *motto.Motto
-	builtIn map[string]otto.Value
+	o *otto.Otto
 }
 
-func (v *VM) AddBuiltinModule(name string, value otto.Value) {
-	v.vm.AddModule(name, v.loader(name))
-}
-
-func (v *VM) loader(name string) func(*motto.Motto) (otto.Value, error) {
-	return func(vm *motto.Motto) (otto.Value, error) {
-		if m, ok := v.builtIn[name]; ok {
-			return m, nil
-		}
-		return otto.Value{}, errors.New("module not found")
-	}
+func NewVM(req *Require) *VM {
+	o := otto.New()
+	o.Set("require", req.ToValue())
+	return &VM{o: o}
 }
