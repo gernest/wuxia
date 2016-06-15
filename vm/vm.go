d9fbf4b8f5a73da75e262a90bc55318b50c7f756
gernest <geofreyernest@live.com>
2016-06-15 20:58:32 +0300

    Add helper methods to wrap otto

--- vm/vm.go
+++ vm/vm.go
@@ -6,6 +6,22 @@ type VM struct {
 	o *otto.Otto
 }
 
+func (vm *VM) Exec(pwd string, src string) (otto.Value, error) {
+	return otto.UndefinedValue(), nil
+}
+
+func (vm *VM) ExecFile(pwd string, src string) (otto.Value, error) {
+	return otto.UndefinedValue(), nil
+}
+
+func (vm *VM) Eval(pwd string, src string) (otto.Value, error) {
+	return otto.UndefinedValue(), nil
+}
+
+func (vm *VM) EvalFile(pwd string, src string) (otto.Value, error) {
+	return otto.UndefinedValue(), nil
+}
+
 func NewVM(req *Require) *VM {
 	o := otto.New()
 	o.Set("require", req.ToValue())
