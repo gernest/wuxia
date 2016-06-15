ed662e2cba3882e7e946a102c08ced758dab2c1c
gernest <geofreyernest@live.com>
2016-06-15 11:16:02 +0300

    Use afero filesystem

--- cmd/gen/gen.go
+++ cmd/gen/gen.go
@@ -3,9 +3,11 @@ package gen
 import (
 	"github.com/gernest/valeria/gen"
 	"github.com/gernest/valeria/vm"
+	"github.com/spf13/afero"
 )
 
 type Generator struct {
 	vm     *vm.VM
 	config *gen.Config
+	fs     afero.Fs
 }
