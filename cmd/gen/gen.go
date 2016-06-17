a1446a4609e1c520df2624c926e6f0bbc3a9e6eb
gernest <geofreyernest@live.com>
2016-06-17 19:31:01 +0300

    Define build steps

--- cmd/gen/gen.go
+++ cmd/gen/gen.go
@@ -11,3 +11,33 @@ type Generator struct {
 	sys *gen.System
 	fs  afero.Fs
 }
+
+func (g *Generator) Build() error {
+	return evaluate(g.init, g.config, g.plan, g.exec, g.down)
+}
+func (g *Generator) init() error {
+	return nil
+}
+func (g *Generator) config() error {
+	return nil
+}
+func (g *Generator) plan() error {
+	return nil
+}
+func (g *Generator) exec() error {
+	return nil
+}
+func (g *Generator) down() error {
+	return nil
+}
+
+func evaluate(fn ...func() error) error {
+	var err error
+	for _, f := range fn {
+		err = f()
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
