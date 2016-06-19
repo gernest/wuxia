1dbb04e7037f344fb3e83749a407d92c273a6289
gernest <geofreyernest@live.com>
2016-06-19 20:13:24 +0300

    Make VM inherit *otto.Otto

--- vm/vm.go
+++ vm/vm.go
@@ -3,27 +3,12 @@ package vm
 import "github.com/robertkrimen/otto"
 
 type VM struct {
-	o *otto.Otto
-}
-
-func (vm *VM) Exec(pwd string, src string) (otto.Value, error) {
-	return otto.UndefinedValue(), nil
-}
-
-func (vm *VM) ExecFile(pwd string, src string) (otto.Value, error) {
-	return otto.UndefinedValue(), nil
-}
-
-func (vm *VM) Eval(pwd string, src string) (otto.Value, error) {
-	return otto.UndefinedValue(), nil
-}
-
-func (vm *VM) EvalFile(pwd string, src string) (otto.Value, error) {
-	return otto.UndefinedValue(), nil
+	*otto.Otto
 }
 
 func NewVM(req *Require) *VM {
-	o := otto.New()
+	o := &VM{}
+	o.Otto = otto.New()
 	o.Set("require", req.ToValue())
-	return &VM{o: o}
+	return o
 }
