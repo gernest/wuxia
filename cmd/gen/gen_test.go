78dae5adb1f18fc4ecaf5025ea209d442a2227a0
gernest <geofreyernest@live.com>
2016-06-22 21:36:42 +0300

    Remove redundant code

--- cmd/gen/gen_test.go
+++ cmd/gen/gen_test.go
@@ -1,13 +1,6 @@
 package gen
 
-import (
-	"encoding/json"
-	"os"
-	"testing"
-
-	"github.com/gernest/valeria/gen"
-	"github.com/spf13/afero"
-)
+import "testing"
 
 func TestGenerator_init(t *testing.T) {
 	g := &Generator{}
@@ -23,56 +16,4 @@ func TestGenerator_init(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-func sampleProjectFs() afero.Fs {
-	data := []struct {
-		path     string
-		mode     os.FileMode
-		contents []byte
-	}{
-		{"README.md", 0600, []byte(readmeFile)},
-		{"config.json", 0600, sampleConfigFile()},
-	}
-	fs := afero.NewMemMapFs()
-	for i := 0; i < len(data); i++ {
-		file, err := fs.OpenFile(data[i].path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, data[i].mode)
-		if err != nil {
-			panic(err)
-		}
-		_, err = file.Write(data[i].contents)
-		if err != nil {
-			panic(err)
-		}
-		if err = file.Close(); err != nil {
-			panic(err)
-		}
-	}
-	return fs
-}
-
-var readmeFile = `
-# v
-Project v is a moderm static content generator for building anything static. It 
-is fast, modular and flexible, combining the power of Go and the easy of use of 
-Javascript.
-`
-
-func sampleConfigFile() []byte {
-	cfg := gen.Config{
-		Source:      "src",
-		Destination: "dest",
-		Safe:        true,
-		Excluede: []string{
-			"tmp/", "bin",
-		},
-		KeepFiles: []string{
-			"LICENCE",
-		},
-		Encoding: "utf-8",
-		Port:     1900,
-		Host:     "localhost",
-	}
-	data, _ := json.Marshal(cfg)
-	return data
 }
