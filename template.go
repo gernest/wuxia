79eb9b444c7a415e3a8d9388f31beff0a97bae73
gernest <geofreyernest@live.com>
2016-07-11 02:09:32 +0300

    Add template + javascript template func support

---
+++ template.go
@@ -0,0 +1,41 @@
+package gen
+
+import (
+	"fmt"
+	"html/template"
+
+	"github.com/robertkrimen/otto"
+)
+
+type Template struct {
+	vm      *otto.Otto
+	jsFuncs []string
+	funcs   template.FuncMap
+	tpl     *template.Template
+}
+
+func (t *Template) funcMap() template.FuncMap {
+	rst := make(template.FuncMap)
+	for _, name := range t.jsFuncs {
+		rst[name] = t.jsTplFunc(name)
+	}
+	for k, v := range t.funcs {
+		rst[k] = v
+	}
+	return rst
+}
+
+func (t *Template) jsTplFunc(name string) func(interface{}) string {
+	return func(arg interface{}) string {
+		call := fmt.Sprintf("Tpl.funcs.%s", name)
+		rst, err := t.vm.Call(call, nil, arg)
+		if err != nil {
+			Panic(err.Error())
+		}
+		if !rst.IsString() {
+			Panic("non string retrun value from " + name + " template func")
+		}
+		s, _ := rst.ToString()
+		return s
+	}
+}
