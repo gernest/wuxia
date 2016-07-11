eab4a41aa64b7c5d8ca0109bb93a62f0504e6c15
gernest <geofreyernest@live.com>
2016-07-11 21:55:58 +0300

    Cleanup

--- scripts.go
+++ scripts.go
@@ -5,9 +5,6 @@ func entryScript() string {
 var system=sys();
 var Tpl={};
 Tpl.funcs={};
-Tpl.funcs.world=function(name){
-	return name+",world"
-}
 Tpl.getTplFuncs=function(){
 	var rst=[]
 	for (var prop in Tpl.funcs){
