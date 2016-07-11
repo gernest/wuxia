de01d92eb27daed54246a7c106d55df10764ffbb
gernest <geofreyernest@live.com>
2016-07-11 21:53:24 +0300

    Add Tpl to the entry script

--- scripts.go
+++ scripts.go
@@ -3,5 +3,19 @@ package gen
 func entryScript() string {
 	return `
 var system=sys();
+var Tpl={};
+Tpl.funcs={};
+Tpl.funcs.world=function(name){
+	return name+",world"
+}
+Tpl.getTplFuncs=function(){
+	var rst=[]
+	for (var prop in Tpl.funcs){
+		if (Tpl.funcs.hasOwnProperty(prop)){
+			rst.push(prop)
+		}
+	}
+	return rst
+}
 `
 }
