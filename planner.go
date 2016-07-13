5ef2760f69de09a0ed3da3d55c0910ca5f30034c
gernest <geofreyernest@live.com>
2016-07-13 06:10:10 +0300

    Move Plan to config.go

--- planner.go
+++ planner.go
@@ -1,16 +1,1 @@
 package wuxia
-
-// Plan is the execution planner object. It states the steps and stages on which
-// the execution process should take.
-type Plan struct {
-	Title string `json:"title"`
-
-	// Modules that are supposed to be loaded before the execution starts. The
-	// execution process wont start if one of the dependencies is missing.
-	Dependency []string `json:"dependencies"`
-
-	TemplateEngine string   `json:"template_engine"`
-	Before         []string `json:"before"`
-	Exec           []string `json:"exec"`
-	After          []string `json:"after"`
-}
