b2949ab5c42318188d04e82dd86e8068d6fdde63
gernest <geofreyernest@live.com>
2016-07-24 10:00:33 +0300

    Add godoc

---
+++ wuxia/doc.go
@@ -0,0 +1,15 @@
+//package wuxia is the modern day static site generator that focus on
+//practical usage , performance , maintainability, and ability to extend.
+//
+// Features
+//
+// * Fast it has been designed for speed in mind. The implementation is well
+// banchmarked.
+//
+// * Robust plugin system. This allows you to write plugins in vanilla
+// javascript. It avoids the pitfall of all Golang based static site generators
+// by allowing uses to tap into the build process via javascript.
+//
+// * Unparalled templating engine. Benefit with the power of go templates with
+// the flexibility of writing template functions in javascript.
+package wuxia
