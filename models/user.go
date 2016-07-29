6a30e763111fc6e54b89ec6e34d906604fab8224
gernest <geofreyernest@live.com>
2016-07-29 18:04:43 +0300

    Add HashPassword
    This uses bcrypt to hash user password

--- models/user.go
+++ models/user.go
@@ -3,6 +3,8 @@ package models
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/gernest/wuxia/db"
 )
 
@@ -17,5 +19,14 @@ type User struct {
 
 func CreateUser(store *db.DB, u *User) error {
 	u.Name = Sanitize(u.Name)
+	p, err := HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = p
 	return nil
+}
+
+func HashPassword(pass []byte) ([]byte, error) {
+	return bcrypt.GenerateFromPassword(pass, bcrypt.DefaultCost)
 }
