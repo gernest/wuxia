cca87994171bd32945af581a9333768c130460f6
gernest <geofreyernest@live.com>
2016-07-29 18:19:38 +0300

    Add test for CreateUser

---
+++ models/user_test.go
@@ -0,0 +1,17 @@
+package models
+
+import "testing"
+
+func TestUser(t *testing.T) {
+	pass := "mypass"
+	uname := "gernest"
+	usr := &User{
+		Name:     uname,
+		Password: []byte(pass),
+	}
+
+	err := CreateUser(store, usr)
+	if err != nil {
+		t.Error(err)
+	}
+}
