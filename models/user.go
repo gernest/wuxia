45d79dd2856bc3c9db0b5d2d04090ea96b8ccad2
gernest <geofreyernest@live.com>
2016-07-30 08:59:59 +0300

    Add FindUserByEmail

--- models/user.go
+++ models/user.go
@@ -26,8 +26,8 @@ func CreateUser(store *db.DB, u *User) error {
 	u.Password = p
 	var query = `
 	BEGIN TRANSACTION;
-	  INSERT INTO users (username,password,email,created_at)
-		VALUES ($1,$2,$3,now());
+	  INSERT INTO users (username,password,email,created_at,updated_at)
+		VALUES ($1,$2,$3,now(),now());
 	COMMIT;
 	`
 	tx, err := store.Begin()
@@ -48,4 +48,23 @@ func HashPassword(pass []byte) ([]byte, error) {
 
 func VerifyPass(hash, pass []byte) error {
 	return bcrypt.CompareHashAndPassword(hash, pass)
+}
+
+func FindUserByEmail(store *db.DB, email string) (*User, error) {
+	var query = `
+	SELECT id(),username,password,email,created_at,updated_at FROM users 
+	WHERE email==$1 ;
+	`
+	u := &User{}
+	err := store.QueryRow(query, email).Scan(
+		&u.ID,
+		&u.Name,
+		&u.Password,
+		&u.Email,
+		&u.CreatedAt,
+		&u.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
 }
--- models/user_test.go
+++ models/user_test.go
@@ -5,9 +5,11 @@ import "testing"
 func TestUser(t *testing.T) {
 	pass := "mypass"
 	uname := "gernest"
+	mail := "hello@test.io"
 	usr := &User{
 		Name:     uname,
 		Password: []byte(pass),
+		Email:    mail,
 	}
 
 	err := CreateUser(store, usr)
@@ -17,5 +19,13 @@ func TestUser(t *testing.T) {
 	err = VerifyPass(usr.Password, []byte(pass))
 	if err != nil {
 		t.Error(err)
+	}
+
+	nusr, err := FindUserByEmail(store, mail)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if nusr.Email != mail {
+		t.Errorf("expected %s got %s", mail, nusr.Email)
 	}
 }
