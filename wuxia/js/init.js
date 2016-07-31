9fa4e8742d67c685db9672bc0745f329a0123f81
gernest <geofreyernest@live.com>
2016-07-31 10:46:03 +0300

    Update process function

--- wuxia/js/init.js
+++ wuxia/js/init.js
@@ -1,6 +1,7 @@
 
 var System=sys();
 var Tpl={};
+var fs=require('fs');
 Tpl.funcs={};
 Tpl.getTplFuncs=function(){
 	var rst=[];
@@ -15,3 +16,20 @@ Tpl.getTplFuncs=function(){
 function getCurrentSys(){
 	return JSON.stringify(System);
 }
+
+function process(fileName){
+  var file ={
+    name: fileName,
+    contents: "",
+    meta:{},
+  };
+  try{
+    var f=sf.open(fileName);
+    file.contents=f.read();
+    f.close();
+  }catch(e){
+    throw e;
+  }
+  return file;
+}
+
