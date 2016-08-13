816bae7553a6ed7f42c2a41a42d4d1557901eea2
gernest <geofreyernest@live.com>
2016-08-13 19:17:53 +0300

    Add struct tags

--- models/project.go
+++ models/project.go
@@ -4,9 +4,9 @@ import "time"
 
 //Project represent a project.
 type Project struct {
-	ID        int
-	Name      string
-	UserID    int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `store:"id"`
+	Name      string    `store:"name"`
+	UserID    int       `store:"user_id"`
+	CreatedAt time.Time `store:"created_at"`
+	UpdatedAt time.Time `store:"updated_at"`
 }
