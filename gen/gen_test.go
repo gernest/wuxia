5496b55df502bca4ee86d724c0bae25ad826239b
gernest <geofreyernest@live.com>
2016-08-27 13:03:00 +0300

    gen: Add test for Configure

--- gen/gen_test.go
+++ gen/gen_test.go
@@ -1,6 +1,9 @@
 package gen
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestGenerator_Build(t *testing.T) {
 	p := "fixture/site"
@@ -25,5 +28,22 @@ func TestGenerator_Build(t *testing.T) {
 	err = Execute(ctx)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestConfigure(t *testing.T) {
+	// default work directory
+	ctx := &Context{}
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ = Configure(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+	if ctx.WorkDir != wd {
+		t.Error("expected %s got %s", wd, ctx.WorkDir)
 	}
 }
