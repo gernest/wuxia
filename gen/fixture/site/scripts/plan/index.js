9c4b1502a25c731501b3b2b17b9bed77a24eba59
gernest <geofreyernest@live.com>
2016-08-28 07:24:31 +0300

    gen: Update fixtures

--- gen/fixture/site/scripts/plan/index.js
+++ gen/fixture/site/scripts/plan/index.js
@@ -1,16 +1,18 @@
-var _plan={};
-_plan.title="simple plan";
-_plan.dependency=[
-  "markdown",
-  "yaml_front_matter",
-];
-_plan.template_engine="go";
-
-_post_strategy={};
-_post_strategy.pattern="*.md";
-_post_strategy.exec=["markdown"];
-_plan.strategies=[_post_strategy];
-
+var _plan={
+    title:"simple plan}",
+    template_engine:"go",
+    dependency:[
+        "markdown",
+        "yaml_front_matter",
+    ],
+    strategies:[
+        {
+            title:"markdown",
+            patterns:["*md"],
+            exec:["markdown"],
+        },
+    ]
+};
 // Register the plan
 System.plan=_plan; 
 
